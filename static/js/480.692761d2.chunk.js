77f0effbb59a6644b73b0d6d0882ce07705de609
Alexandra-Makarenko <37274881+Alexandra-Makarenko@users.noreply.github.com>
2023-02-04 16:19:45 +0000

    Deploying to gh-pages from @ Alexandra-Makarenko/pets-support-team-project@ed4a4338edc61775ad289c3bf2ca18b1b11f63ad 🚀

--- static/js/480.692761d2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[480],{480:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("section",{children:(0,c.jsx)("h2",{children:"RegisterPage"})})}}}]);
-//# sourceMappingURL=480.692761d2.chunk.js.map
