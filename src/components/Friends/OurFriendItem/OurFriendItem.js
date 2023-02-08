4c6a76f89cd50c10acf8125623c62dd4b482532a
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2023-02-08 17:07:35 +0200

    remove temporary comments

--- src/components/Friends/OurFriendItem/OurFriendItem.js
+++ src/components/Friends/OurFriendItem/OurFriendItem.js
@@ -11,7 +11,6 @@ import {
   ImgWrapper,
   HoursOfWeek,
   HoursWrapper,
-  // HoursItem,
 } from './OurFriendItem.styled.js';
 
 import getHours from './getHours';
@@ -80,24 +79,3 @@ export const OurFriendItem = ({
     </FriendItem>
   );
 };
-
-// export default OurFriendItem;
-/*
-    "title": "Притулок для бездомних тварин 'Сіріус'",
-    "url": "https://dogcat.com.ua",
-    "addressUrl": "https://goo.gl/maps/iq8NXEUf31EAQCzc6",
-    "imageUrl": "https://storage.googleapis.com/kidslikev2_bucket/pets-support/images/sponsors/frame_287.png",
-    "address": "Fedorivka, Kyiv Oblast, Ukraine, 07372",
-    "workDays": [
-      { "isOpen": false },
-      { "isOpen": false },
-      { "isOpen": false },
-      { "isOpen": false },
-      { "isOpen": false },
-      { "isOpen": true, "from": "11:00", "to": "16:00" },
-      { "isOpen": true, "from": "11:00", "to": "16:00" }
-    ],
-    "phone": "+380931934069",
-    "email": null
-  },
- */
