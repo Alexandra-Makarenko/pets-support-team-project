5a630ba9e8d5337ff9d13ab6dec1f1368f4b5d01
Alexandra-Makarenko <s850272@gmail.com>
2023-02-08 11:32:18 +0200

    npm i react-phone-input-2

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -3,29 +3,29 @@ import styled from 'styled-components';
 import { device } from '../../styles/device';
 
 export const SharedLayoutBox = styled.div`
-  /* background-color: ${props => props.theme.backgrounds.input};
+  background-color: ${props => props.theme.backgrounds.input};
   width: 100%;
-  min-height: 100vh; */
+  min-height: 100vh; 
 `;
 export const SharedLayoutContainer = styled.div`
-  /* outline: 1px solid red;
+  outline: 1px solid red;
   margin-left: auto;
   margin-right: auto;
   width: 320px;
   padding-top: 16px;
   padding-left: 20px;
-  padding-right: 20px; */
+  padding-right: 20px;
 
   @media ${device.tablet} {
-    /* width: 768px;
+    width: 768px;
     padding-top: 24px;
     padding-left: 32px;
-    padding-right: 32px; */
+    padding-right: 32px;
   }
   @media ${device.desktop} {
-    /* width: 1280px;
+    width: 1280px;
     padding-top: 20px;
     padding-left: 16px;
-    padding-right: 16px; */
+    padding-right: 16px;
   }
 `;
