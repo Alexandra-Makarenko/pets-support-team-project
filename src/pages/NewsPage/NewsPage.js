257ca9483d870da183ae10d2b54449338b9b131c
Vasilina Biznyakova <vbiznyakova@gmail.com>
2023-02-08 11:51:16 +0100

    add component for search, folder for logo

--- src/pages/NewsPage/NewsPage.js
+++ src/pages/NewsPage/NewsPage.js
@@ -1,7 +1,8 @@
-
 import { useState, useEffect } from 'react';
-import Searchbar from './components/Searchbar';
+// import Searchbar from './components/Searchbar';
+import { SearchForm } from 'components/Search/SearchForm';
 import NewsFeed from './components/NewsFeed';
+import { PagesTitle } from 'components/PagesTitle/PagesTitle';
 import { fetchNews } from './components/helpers/fetchNews';
 
 const NewsPage = () => {
@@ -28,15 +29,15 @@ const NewsPage = () => {
 
   return (
     <section>
-
-      <Searchbar
+      <PagesTitle>News</PagesTitle>
+      <SearchForm />
+      {/* <Searchbar
         onSubmit={handleSubmit}
         onChange={updateQueryString}
         value={search}
-      />
+      /> */}
 
       <NewsFeed news={news} />
-
     </section>
   );
 };
