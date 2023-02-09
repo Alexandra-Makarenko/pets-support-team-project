8a4cc5f55b0c17f3221772cca6a1c4954bc9ad23
Alexandra-Makarenko <s850272@gmail.com>
2023-02-09 18:06:18 +0200

    categories render

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -1,19 +1,18 @@
 import { Header } from "../Header/Header";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { SharedLayoutBox, SharedLayoutContainer } from "./SharedLayout.styled";
+import { SharedLayoutBox } from "./SharedLayout.styled";
 
 
 
 export const SharedLayout = () => {
   return (
     <SharedLayoutBox>
-      <SharedLayoutContainer>
       <Header />
           <Suspense fallback={<div>Loading...</div>}>
               <Outlet />
           </Suspense> 
-        </SharedLayoutContainer>       
+     
     </SharedLayoutBox>
   );
 };
--- src/redux/petsSlice.js
+++ src/redux/petsSlice.js
@@ -1,40 +1,30 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
-import  petsdata  from '../data/pets.json';
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchTasks } from "./operations";
 
-const tasksInitialState = petsdata;
+// const tasksInitialState = petsdata;
 
 const petsSlice = createSlice({
-  name: "tasks",
-  initialState: tasksInitialState,
-  reducers: {
-    addTask: {
-      reducer(state, action) {
-        state.push(action.payload);
-      },
-      prepare(text) {
-        return {
-          payload: {
-            text,
-            id: nanoid(),
-            completed: false,
-          },
-        };
-      },
+  name: "pets",
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers: {
+    [fetchTasks.pending](state) {
+      state.isLoading = true;
     },
-    deleteTask(state, action) {
-      const index = state.findIndex(task => task.id === action.payload);
-      state.splice(index, 1);
+    [fetchTasks.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
     },
-    toggleCompleted(state, action) {
-      for (const task of state) {
-        if (task.id === action.payload) {
-          task.completed = !task.completed;
-          break;
-        }
-      }
+    [fetchTasks.rejected](state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
     },
   },
 });
 
-export const { addTask, deleteTask, toggleCompleted } = petsSlice.actions;
+// export const { addTask, deleteTask, toggleCompleted } = petsSlice.actions;
 export const petsReducer = petsSlice.reducer;
