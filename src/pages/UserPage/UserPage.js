bf0ce38fd9484e7e5ceaa9635badfedf79a6bdd3
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2023-02-10 18:48:56 +0200

    Update UserPage.js

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -9,10 +9,56 @@ import {
 import emptyPhoto from './emptyPhoto.jpg';
 import { useSelector } from 'react-redux';
 import { selectUser } from 'redux/auth/authSelectors';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+axios.defaults.baseURL = 'http://localhost:3018/api';
+const fetchPets = async () => {
+  const res = await axios.get('user');
+  return res.data.result.pets;
+};
 
 const UserPage = () => {
+  const [pets, setPets] = useState('');
   const { email, name, place, phone, dayofbirth, avatarURL } =
     useSelector(selectUser);
+
+  useEffect(() => {
+    async function getPets() {
+      const data = await fetchPets();
+      setPets(
+        data.length > 0
+          ? data.map(({ name, date, breed, comment, avatarURL }) => {
+              return (
+                <div>
+                  <img
+                    src={avatarURL || ''}
+                    width={161}
+                    height={161}
+                    alt="pet"
+                  />
+                  <ul>
+                    <li>
+                      <p>Name: {name || ''}</p>
+                    </li>
+                    <li>
+                      <p>Date of birth: {date || ''}</p>
+                    </li>
+                    <li>
+                      <p>Breed: {breed || ''}</p>
+                    </li>
+                    <li>
+                      <p>Comments: {comment || ''}</p>
+                    </li>
+                  </ul>
+                </div>
+              );
+            })
+          : null
+      );
+    }
+    getPets();
+  }, []);
   const Li = (fieldName, value, isActive) => (
     <li>
       <FieldName>{fieldName}</FieldName>
@@ -44,9 +90,8 @@ const UserPage = () => {
           {Li('City:', place, true)}
         </ul>
       </MyInfoSection>
-      <section>
-        <h2>My pets</h2>
-      </section>
+      <h2>My pets</h2>
+      <section>{pets}</section>
     </>
   );
 };
