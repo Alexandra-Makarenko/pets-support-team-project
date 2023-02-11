c5bc84d9ded4477b311c091c47718d0583813d61
Alexandr Sorokin <sanek1678@gmail.com>
2023-02-11 04:42:20 +0200

    Added AddNoticeModal and Buttons
    Додає модалку для нотісів і відповідну кнопку.
    Модалка повністю функціональна але поки не відправляє реквест

--- src/components/Modals/ModalNotice/ModalNotice.js
+++ src/components/Modals/ModalNotice/ModalNotice.js
@@ -1,8 +1,51 @@
-export const ModalNotice = () => {
+export const ModalNotice = (notice, user) => {
   return (
     <section>
-      <h2>ModalNotice must be here</h2>
-      
+      <h3>{notice.category}</h3>
+      <img src={notice.imageUrl} alt={notice.name} />
+      <h4>Cute pet looking for a home</h4>
+      <ul>
+        <li>
+          <h5>Name:</h5>
+          <p>{notice.name}</p>
+        </li>
+        <li>
+          <h5>Birthday:</h5>
+          <p>{notice.dateofbirth}</p>
+        </li>
+        <li>
+          <h5>Breed:</h5>
+          <p>{notice.breed}</p>
+        </li>
+        <li>
+          <h5>Location:</h5>
+          <p>{notice.adress}</p>
+        </li>
+        <li>
+          <h5>Sex:</h5>
+          <p>{notice.sex ? 'Male' : 'Female'}</p>
+        </li>
+        <li>
+          <h5>Email:</h5>
+          <p>{user.email}</p>
+        </li>
+        <li>
+          <h5>Phone:</h5>
+          <p>{user.phone}</p>
+        </li>
+        <li>
+          <h5>Price:</h5>
+          <p>{notice.price}</p>
+        </li>
+      </ul>
+      <span>
+        <h5>Comments:</h5>
+        <p>{notice.comment}</p>
+      </span>
+      <button>Contact</button>
+      <button>
+        Add to <img />
+      </button>
     </section>
   );
 };
--- src/pages/NoticesPage/NoticesPage.js
+++ src/pages/NoticesPage/NoticesPage.js
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
@@ -7,14 +7,21 @@ import { SearchForm } from 'components/Search/SearchForm';
 import { Container } from '../../components/Container/Container';
 import { NoticesCategoriesNav } from '../../components/Notices/NoticesCategoriesNav/NoticesCategoriesNav';
 import { PagesTitle } from '../../components/PagesTitle/PagesTitle';
-import { AddNoticeButton } from '../../components/Notices/AddNoticeButton/AddNoticeButton';
 import { NavBox, TitleBox } from './NoticesPage.styled';
 import { Loader } from 'components/Loader/Loader';
 import { getNoticesIsLoading, getNoticesError } from 'redux/notices/selectors';
-
+import AddNoticeButton from 'components/Notices/AddNoticeButton/AddNoticeButton';
+import { ModalAddNotice } from 'components/Modals/ModalAddNotice/ModalAddNotice';
 const NoticesPage = () => {
+  const [isAddNoticeOpen, setIsAddNoticeOpen] = useState(false);
+
   const isLoading = useSelector(getNoticesIsLoading);
   const error = useSelector(getNoticesError);
+
+  const toggleAddNoticeModal = () => {
+    setIsAddNoticeOpen(!isAddNoticeOpen);
+    console.log(isAddNoticeOpen);
+  };
 
   return (
     <Container>
@@ -24,15 +31,16 @@ const NoticesPage = () => {
       <SearchForm mbtn={{ mobile: 28, rest: 40 }} />
       <NavBox>
         <NoticesCategoriesNav />
-        <AddNoticeButton />
+        <AddNoticeButton onClick={toggleAddNoticeModal} />
       </NavBox>
 
       {isLoading && !error && <Loader />}
       {error &&
         !isLoading &&
         toast.error(`Something wrong, please try again later: ${error}`)}
-
       <Suspense fallback={<div>Loading subpage...</div>}>
+        {isAddNoticeOpen && <ModalAddNotice onClick={toggleAddNoticeModal} />}
+
         <Outlet />
       </Suspense>
     </Container>
