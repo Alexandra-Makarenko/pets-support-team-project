7d383d419c82e4fb4162eb609524905a3ece0439
Alexandra-Makarenko <s850272@gmail.com>
2023-02-12 21:35:45 +0200

    ModalNotice

--- src/components/Modals/ModalNotice/ModalNotice.js
+++ src/components/Modals/ModalNotice/ModalNotice.js
@@ -1,53 +1,87 @@
+import {
+  ModalNoticeBox,
+  Wrap,
+  ImgWrap,
+  CategoryLabel,
+  Img,
+  WrapInner,
+  Title,
+  Ul,
+  Li,
+  Lable,
+  Text,
+  CommentsParagraph,
+  CommentsLable,
+  ContactModalNoticeBtn,
+  AddModalNoticeBtn,
+  HeartIcon,
+} from './ModalNotice.styled';
 
-export const ModalNotice = (notice, user) => {
+import { useSelector } from 'react-redux';
+import { getOneNotice } from 'redux/notices/selectors';
+
+export const ModalNotice = () => {
+  const oneNotice = useSelector(getOneNotice);
+  console.log(oneNotice);
+
   return (
-    <section>
-      <h3>{notice.category}</h3>
-      <img src={notice.imageUrl} alt={notice.name} />
-      <h4>Cute pet looking for a home</h4>
-      <ul>
-        <li>
-          <h5>Name:</h5>
-          <p>{notice.name}</p>
-        </li>
-        <li>
-          <h5>Birthday:</h5>
-          <p>{notice.dateofbirth}</p>
-        </li>
-        <li>
-          <h5>Breed:</h5>
-          <p>{notice.breed}</p>
-        </li>
-        <li>
-          <h5>Location:</h5>
-          <p>{notice.adress}</p>
-        </li>
-        <li>
-          <h5>Sex:</h5>
-          <p>{notice.sex ? 'Male' : 'Female'}</p>
-        </li>
-        <li>
-          <h5>Email:</h5>
-          <p>{user.email}</p>
-        </li>
-        <li>
-          <h5>Phone:</h5>
-          <p>{user.phone}</p>
-        </li>
-        <li>
-          <h5>Price:</h5>
-          <p>{notice.price}</p>
-        </li>
-      </ul>
-      <span>
-        <h5>Comments:</h5>
-        <p>{notice.comment}</p>
-      </span>
-      <button>Contact</button>
-      <button>
-        Add to <img alt="img"/>
-      </button>
-    </section>
-
+    <ModalNoticeBox>
+      <ImgWrap>
+        <CategoryLabel>{oneNotice.category}</CategoryLabel>
+        <Img src={oneNotice.avatarURL} alt={oneNotice.name} loading="lazy" />
+        {/* <FavoriteBtn/> */}
+      </ImgWrap>
+      <Wrap>
+        <WrapInner>
+          <Title>{oneNotice.title || 'Title must be here'}</Title>
+          <Ul>
+            <Li key={`${oneNotice._id}+name`}>
+              <Lable>Name:</Lable>
+              <Text>{oneNotice.name || '----------'}</Text>
+            </Li>
+            <Li key={`${oneNotice._id}+dateofbirth`}>
+              <Lable>Birthday:</Lable>
+              <Text>{oneNotice.dateofbirth}</Text>
+            </Li>
+            <Li key={`${oneNotice._id}+breed`}>
+              <Lable>Breed:</Lable>
+              <Text>{oneNotice.breed}</Text>
+            </Li>
+            <Li key={`${oneNotice._id}+place`}>
+              <Lable>Location:</Lable>
+              <Text>{oneNotice.place}</Text>
+            </Li>
+            <Li key={`${oneNotice._id}+sex`}>
+              <Lable>The sex:</Lable>
+              <Text>{oneNotice.sex || '----------'}</Text>
+            </Li>
+            <Li key={`${oneNotice._id}+email`}>
+              <Lable>Email:</Lable>
+              <Text>{oneNotice.email || '----------'}</Text>
+            </Li>
+            <Li key={`${oneNotice._id}+phone`}>
+              <Lable>Phone:</Lable>
+              <Text>{oneNotice.phone || '----------'}</Text>
+            </Li>
+            {oneNotice.category === 'sell' && (
+              <Li key={`${oneNotice._id}+sell`}>
+                <Lable>Price:</Lable>
+                <Text>{oneNotice.price || '----------'}</Text>
+              </Li>
+            )}
+          </Ul>
+          <CommentsParagraph>
+            <CommentsLable>Comments: </CommentsLable>
+            {oneNotice.comments || ''}
+          </CommentsParagraph>
+        </WrapInner>
+      </Wrap>
+      <ContactModalNoticeBtn type="tel" onClick={() => {}}>
+        Contact
+      </ContactModalNoticeBtn>
+      <AddModalNoticeBtn type="button" onClick={() => {}}>
+        Add to <HeartIcon />
+      </AddModalNoticeBtn>
+    </ModalNoticeBox>
   );
 };
