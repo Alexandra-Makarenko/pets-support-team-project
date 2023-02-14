5455c17c8418e21cf99e77804b8be80a7404708a
Alexandr Sorokin <sanek1678@gmail.com>
2023-02-14 02:30:17 +0200

    Added styles for button and modal window
    Стилі додано

--- src/components/Notices/AddNoticeButton/AddNoticeButton.styled.js
+++ src/components/Notices/AddNoticeButton/AddNoticeButton.styled.js
@@ -1,18 +1,57 @@
 import styled from 'styled-components';
 import { device } from '../../../styles/device';
-
-const AddNotice = styled.button`
+import { BsPlusLg } from 'react-icons/bs';
+export const AddNotice = styled.button`
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
   width: 80px;
-  height: 80px;
+  line-height: 16px;
+  height: 45px;
   border-radius: 40px;
   border: none;
-  background-color: #f59256;
   cursor: pointer;
+  width: 25%;
   &:hover {
     transform: scale(1.1);
   }
+  &:active {
+    transform: scale(1.1);
+  }
   @media ${device.preTablet} {
-    display: none;
+    position: fixed;
+    right: 20px;
+    bottom: 80px;
+    z-index: 100;
+    display: initial;
+    background-color: #f59256;
   }
 `;
-export { AddNotice };
+export const AddNoticeButtonText = styled.p`
+  display: inline-block;
+  white-space: nowrap;
+  font-family: 'Manrope500';
+  font-size: 20 px;
+  line-height: 27px;
+  margin-right: 20px;
+
+  @media ${device.preTablet} {
+    color: #ffffff;
+    font-size: 12px;
+  }
+`;
+export const BsPlusLgButtonContainer = styled.div`
+  padding: 10px;
+  background-color: #f59256;
+  border-radius: 50%;
+`;
+export const BsPlusLgButton = styled(BsPlusLg)`
+  width: 24px;
+  height: 24px;
+  color: #ffffff;
+
+  @media ${device.preTablet} {
+    width: 32px;
+    height: 32px;
+  }
+`;
