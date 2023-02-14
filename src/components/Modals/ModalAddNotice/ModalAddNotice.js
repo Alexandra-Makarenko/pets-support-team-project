25b50b37576a2ea57f3a532a150350f3169efa72
Alexandra-Makarenko <37274881+Alexandra-Makarenko@users.noreply.github.com>
2023-02-14 22:43:18 +0200

    Update ModalAddNotice.js
    const dateRegEx =
      /^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/;

--- src/components/Modals/ModalAddNotice/ModalAddNotice.js
+++ src/components/Modals/ModalAddNotice/ModalAddNotice.js
@@ -33,8 +33,7 @@ import {
 
 const nameRules = /^[aA-zZ\s]+$/;
 const regionRules = /^()(\w+(,|\s)\s*)+\w+$/;
-const dateRegEx =
-  /^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/;
+
 
 export const ModalAddNotice = ({ onClick, isOpen }) => {
   const [data, setData] = useState({
