a1ceafe66b9251d6d4a1670c9d6b13eb36a7add0
Dmytro Yanko <yanko.dmitriy@gmail.com>
2023-02-15 16:30:38 +0200

    Active button style fix

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,15 +1,32 @@
+import { useLocation } from 'react-router-dom';
 import { Button } from 'components/Button/Button.styled';
 import { AuthNavBtn } from './AuthNav.styled';
 
 export const AuthNav = mobMenuToggler => {
+  const { pathname } = useLocation();
+  console.log(pathname === '/register');
+
   return (
     <>
-      <AuthNavBtn to="login" onClick={mobMenuToggler}>
-        <Button primary>Login</Button>
-      </AuthNavBtn>
-      <AuthNavBtn to="register" onClick={mobMenuToggler}>
-        <Button>Registration</Button>
-      </AuthNavBtn>
+      {pathname === '/register' ? (
+        <AuthNavBtn to="login" onClick={mobMenuToggler}>
+          <Button>Login</Button>
+        </AuthNavBtn>
+      ) : (
+        <AuthNavBtn to="login" onClick={mobMenuToggler}>
+          <Button primary>Login</Button>
+        </AuthNavBtn>
+      )}
+
+      {pathname === '/register' ? (
+        <AuthNavBtn to="register" onClick={mobMenuToggler}>
+          <Button primary>Registration</Button>
+        </AuthNavBtn>
+      ) : (
+        <AuthNavBtn to="register" onClick={mobMenuToggler}>
+          <Button>Registration</Button>
+        </AuthNavBtn>
+      )}
     </>
   );
 };
