2916151aa039ccbf61cfc2f53c86ca182e74b674
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2023-02-16 18:56:30 +0200

    add Modal to OurFriends

--- src/components/Friends/OurFriends/OurFriends.js
+++ src/components/Friends/OurFriends/OurFriends.js
@@ -2,15 +2,17 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 import { OurFriendItem } from 'components/Friends';
-import { FriendsList } from './OurFriends.styled.js';
 import { Loader } from 'components/Loader/Loader';
+import { FriendsList } from './OurFriends.styled.js';
 
-// import SPONSORS from 'data/sponsors.json';
-// import axios from 'axios';
 import { fetchFriends } from 'components/Friends/services';
+import { FriendModal } from 'components/Friends/FriendModal';
 
 export const OurFriends = () => {
   const [friends, setFriends] = useState([]);
+  const [modalUrl, setModalUrl] = useState('');
+
+  const toggleModal = modalUrl => setModalUrl(modalUrl ?? '');
 
   const getFriends = async () => {
     try {
@@ -29,11 +31,18 @@ export const OurFriends = () => {
       {!friends.length ? (
         <Loader />
       ) : (
-        <FriendsList>
-          {friends.map(friend => (
-            <OurFriendItem key={friend._id} {...friend} />
-          ))}
-        </FriendsList>
+        <>
+          <FriendsList>
+            {friends.map(friend => (
+              <OurFriendItem
+                key={friend._id}
+                toggleModal={toggleModal}
+                {...friend}
+              />
+            ))}
+          </FriendsList>
+          {modalUrl && <FriendModal url={modalUrl} toggleModal={toggleModal} />}
+        </>
       )}
     </>
   );
