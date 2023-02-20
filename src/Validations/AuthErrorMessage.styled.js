a7526cdaf02a02c8e85735e6d15a20aabf6170b9
Dmytro Yanko <yanko.dmitriy@gmail.com>
2023-02-20 22:37:13 +0200

    form error with fix

--- src/Validations/AuthErrorMessage.styled.js
+++ src/Validations/AuthErrorMessage.styled.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
+import { device } from 'styles/device';
 
 export const ErrorText = styled.p`
   font-family: 'Manrope400';
   display: block;
+  max-width: 240px;
   font-size: 14px;
   line-height: 18px;
   letter-spacing: 0.04em;
   margin-top: 4px;
+  margin-bottom: 8px;
   color: red;
+
+  @media ${device.tablet} {
+    max-width: 448px;
+  }
 `;
